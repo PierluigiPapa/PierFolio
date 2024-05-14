d4ad02fbb9e93396464c36841d424b5dc5f816df
PierluigiPapa <pierluigipapa9@gmail.com>
2024-05-14 17:52:20 +0200

    feat: added header

--- src/components/header/AppHeader.vue
+++ src/components/header/AppHeader.vue
@@ -1,4 +1,5 @@
 <script>
+
 export default {
     name: 'AppHeader',
 }
@@ -7,41 +8,61 @@ export default {
 <template>
   <nav class="navbar navbar-expand-lg">
     <div class="container-fluid">
-      <a class="navbar-brand" href="#">Navbar</a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
+      <!-- // SECTION LOGO_NAVBAR // -->
+      <div class="navbar-header">
+        <a class="navbar-brand" href="#">PierFolio</a>
+      </div>
+      <!-- // SECTION LOGO_NAVBAR // -->
 
-      <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav">
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">Home</a>
-          </li>
+      <!-- // SECTION LINK_NAVBAR_RIGHT // -->
+      <div class="navbar-right">
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link" href="#">Chi sono</a>
+            </li>
 
-          <li class="nav-item">
-            <a class="nav-link" href="#">Features</a>
-          </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Portfolio</a>
+            </li>
 
-          <li class="nav-item">
-            <a class="nav-link" href="#">Pricing</a>
-          </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Progetti</a>
+            </li>
 
-          <li class="nav-item">
-            <a class="nav-link disabled" aria-disabled="true">Disabled</a>
-          </li>
-        </ul>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Competenze</a>
+            </li>
+
+            <li class="nav-item">
+              <a class="nav-link" href="#">Contatti</a>
+            </li>
+          </ul>
+        </div>
+        <!-- // SECTION LINK_NAVBAR_RIGHT // -->
       </div>
     </div>
   </nav>
-
- 
 </template>
+
+
 
 <style lang="scss">
 @use "../../styles/partials/variables.scss" as *;
 
 nav {
   font-family: "Poppins", sans-serif;
+  background-color: #525252;
+
+  .nav-link,
+  .navbar-brand {
+    color: white;
+  }
+
+  .nav-link:hover,
+  .navbar-brand:hover {
+    color: black;
+  }
 }
 
 </style>
--- src/main.js
+++ src/main.js
@@ -2,5 +2,13 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import 'bootstrap/dist/css/bootstrap.css';
 
+import { library } from "@fortawesome/fontawesome-svg-core";
 
-createApp(App).mount('#app')
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
+
+library.add(faLinkedin);
+
+createApp(App).component("font-awesome-icon", FontAwesomeIcon).mount("#app");
+
