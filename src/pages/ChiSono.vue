01459c32e9614da2bb9762f9f81a627257a52f99
PierluigiPapa <pierluigipapa9@gmail.com>
2024-05-30 00:30:24 +0200

    feat: added text in ChiSono.vue

--- src/pages/ChiSono.vue
+++ src/pages/ChiSono.vue
@@ -18,11 +18,17 @@ export default {
       <div class="col-6 d-flex justify-content-center ps-5 mt-5">
         <div>
           <h6 class="text-light fs-5 mb-5">Sono nato a <span class="gray fw-bold fs-5">Brindisi</span>, 
-            una città marittima dell' <span class="gray fw-bold fs-5">Italia</span> meridionale, in un giorno d'estate del <span class="gray fw-bold fs-5">1992</span>. 
-            Dopo aver conseguito il <span class="gray fw-bold fs-5">diploma</span> di <span class="gray fw-bold fs-5">maturità scientifica</span>, mi sono iscritto alla facoltà
-            di Beni Culturali a Lecce, laureandomi, spinto dalla passione per la storia, in particolare per quella medievale e per la cultura. Qui, ho avuto il primo contatto con il mondo
-            della programmazione web creando un piccolo sito web, per il corso di Informatica, Brindisi Medievale, incentrato sullo studio del Medioevo, nella provincia brindisina.
-            Dopodiché, ho conseguito la laurea magistrale in Scienze storiche e della documentazione storica a Bari.
+            una città marittima dell' <span class="gray fw-bold fs-5">Italia</span> meridionale, nell'estate del <span class="gray fw-bold fs-5">1992</span>. 
+            Dopo aver preso il <span class="gray fw-bold fs-5">diploma</span> di <span class="gray fw-bold fs-5">maturità scientifica</span>, inizio gli studi 
+            <span class="gray fw-bold fs-5">universitari</span>, iscrivendomi alla facoltà di <span class="gray fw-bold fs-5">Beni Culturali</span> di 
+            <span class="gray fw-bold fs-5">Lecce</span>, spinto dalla <span class="gray fw-bold fs-5">passione</span> per la <span class="gray fw-bold fs-5">storia</span>, 
+            in particolare per quella <span class="gray fw-bold fs-5">medievale</span> e per la <span class="gray fw-bold fs-5">cultura</span>. Qui, ho avuto il primo 
+            <span class="gray fw-bold fs-5">approccio</span> con il mondo della <span class="gray fw-bold fs-5">programmazione web</span>, creando un piccolo portale, 
+            per il corso di <span class="gray fw-bold fs-5">Informatica</span>, <span class="gray fst-italic fw-bold font-italic fs-5">Brindisi Medievale</span>, 
+            incentrato sullo <span class="gray fw-bold fs-5">studio</span> del <span class="gray fw-bold fs-5">Medioevo</span>, nella provincia brindisina. 
+            Dopo aver concluso questi studi, mi sono iscritto al <span class="gray fw-bold fs-5">Corso di Laurea Magistrale</span> in 
+            <span class="gray fw-bold fs-5">Scienze Storiche e della documentazione storica</span> di <span class="gray fw-bold fs-5">Bari</span>, laureandomi
+            nel pieno dell'emergenza sanitaria del <span class="gray fw-bold fs-5">Covid-19</span>.
           </h6>
         </div>
       </div>
@@ -39,9 +45,18 @@ export default {
 
       <div class="col-6 d-flex justify-content-center mt-5">
         <div>
-          <h6 class="text-light fs-5 mb-5">Essendo appassionato di tecnologia e soprattutto alla ricerca di un ambientee lavorativo stimolante e dinamico, ho deciso di iscrivermi
-            al corso in Full-Stack Web Development, dalla durata di sei mesi, dove ho imparato alcuni dei più importanti linguaggi di programmazione e framework, sia Front End sia 
-            Back End: HTML, CSS, JavaScript, Bootstrap, VueJS, Vite, SASS, PHP, MySQL e ho realizzato dei progetti stimolanti, sia singoli sia di gruppo.
+          <h6 class="text-light pe-5 fs-5 mb-5">Essendo <span class="blue fw-bold fs-5">appassionato</span> di <span class="blue fw-bold fs-5">tecnologia</span> e soprattutto 
+            alla <span class="blue fw-bold fs-5">ricerca</span> di un <span class="blue fw-bold fs-5">ambiente lavorativo stimolante</span> e 
+            <span class="blue fw-bold fs-5">dinamico</span>, ho deciso di iscrivermi al corso in <span class="blue fw-bold fs-5">Full-Stack Web Development</span> 
+            di <span class="blue fw-bold fs-5">Boolean</span>, dalla durata di sei mesi. È stata un'esperienza <span class="blue fw-bold fs-5">formativa</span>, non solo perché 
+            ho imparato alcuni dei più importanti <span class="blue fw-bold fs-5">linguaggi di programmazione</span> e 
+            <span class="blue fw-bold fs-5">framework front-end e back-end</span>: <span class="blue fw-bold fs-5">HTML5</span>, <span class="blue fw-bold fs-5">CSS</span>, 
+            <span class="blue fw-bold fs-5">JavaScript</span>,<span class="blue fw-bold fs-5">Bootstrap</span>, <span class="blue fw-bold fs-5">VueJS</span>, 
+            <span class="blue fw-bold fs-5">Vite</span>, <span class="blue fw-bold fs-5">SASS</span>, <span class="blue fw-bold fs-5">PHP</span>, 
+            <span class="blue fw-bold fs-5">MySQL</span>, <span class="blue fw-bold fs-5">Laravel</span>, ma anche a livello <span class="blue fw-bold fs-5">professionale</span>, 
+            in quanto la sua intensità, mi ha fatto comprendere cosa significa <span class="blue fw-bold fs-5">essere</span> un programmatore web, grazie anche agli 
+            <span class="blue fw-bold fs-5">esercizi</span>esercizi e ai <span class="blue fw-bold fs-5">progetti</span>, svolti 
+            <span class="blue fw-bold fs-5">singolarmente</span> o <span class="blue fw-bold fs-5">in team</span>.
           </h6>
         </div>
       </div>
@@ -50,9 +65,20 @@ export default {
     <div class="row">
       <div class="col-6 d-flex justify-content-center ps-5 mt-5">
         <div>
-          <h6 class="text-light fs-5 mb-5">Sono un ragazzo dinamico, curioso, che lavorare in team, attenta ai dettagli e organizzata. Nella vita privata, mi dedico con passione 
-            a viaggiare, leggere e fotografare, trovando ispirazione e arricchimento personale in queste esperienze. Inoltre, mi diverto a risolvere problemi complessi 
-            attraverso videogiochi e giochi da tavolo, oltre a dedicarmi con precisione e rigore alla pasticceria e alla pizzeria, praticando la mia creatività.
+          <h6 class="text-light fs-5 mb-5">Alla domanda: <span class="gray fw-bold fst-italic fs-5">"Come ti descriveresti?"</span> Risponderei che sono un 
+            ragazzo <span class="gray fw-bold fs-5">dinamico</span>, <span class="gray fw-bold fs-5">curioso</span>, <span class="gray fw-bold fs-5">socievole</span>, 
+            <span class="gray fw-bold fs-5">creativo</span>, a cui piace <span class="gray fw-bold fs-5">lavorare in team</span> e 
+            <span class="gray fw-bold fs-5">attento ai dettagli</span>. Mi dedico con <span class="gray fw-bold fs-5">passione</span> a <span class="gray fw-bold fs-5">viaggiare</span>, 
+            <span class="gray fw-bold fs-5">leggere</span> e <span class="gray fw-bold fs-5">fotografare</span>, trovando  <span class="gray fw-bold fs-5">ispirazione</span> e 
+            <span class="gray fw-bold fs-5">arricchimento personale</span> in queste esperienze, perché  <span class="gray fw-bold fs-5">entrare in contatto</span> con 
+            <span class="gray fw-bold fs-5">altre culture</span> fa <span class="gray fw-bold fs-5">aprire la mente</span> e a 
+            <span class="gray fw-bold fs-5">nuovi orizzonti</span>. Mi piace <span class="gray fw-bold fs-5">scrivere</span> articoli sul Medioevo e non solo, 
+            <span class="gray fw-bold fs-5">filmare</span> tutto ciò che i miei occhi <span class="gray fw-bold fs-5">osservano</span>. Adoro 
+            <span class="gray fw-bold fs-5">praticare sport</span>, giocando a <span class="gray fw-bold fs-5">quadball</span>, a <span class="gray fw-bold fs-5">calcetto</span> 
+            con gli <span class="gray fw-bold fs-5">amici</span>, a <span class="gray fw-bold fs-5">pallacanestro</span> e <span class="gray fw-bold fs-5">allenandomi</span>, in 
+            <span class="gray fw-bold fs-5">palestra</span>. Inoltre, mi <span class="gray fw-bold fs-5">diverto</span> a <span class="gray fw-bold fs-5">giocare</span> ai 
+            <span class="gray fw-bold fs-5">videogiochi</span> e ai <span class="gray fw-bold fs-5">giochi da tavolo</span>, stimolando la 
+            <span class="gray fw-bold fs-5">logica</span>.
           </h6>
         </div>
       </div>
@@ -70,6 +96,7 @@ export default {
 
 .container-chi {
   background-color: #1D70A2;
+  text-align: justify;
 
   .hello {
     font-size: 70px;
